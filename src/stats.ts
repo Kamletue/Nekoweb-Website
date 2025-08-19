a02c10d5d1f9cfb77d7e308647395a2c925affca
Kamletue <>
2025-08-19 21:11:43 +0200

    New "stats.ts" script to fetch statistics from Nekoweb API. Added misc CSS code. Also added "@types/node".

---
+++ src/stats.ts
@@ -0,0 +1,43 @@
+let url = 'https://nekoweb.org/api/site/info/kamuretsu.nekoweb.org';
+//let url = "https://jsonplaceholder.typicode.com/todos/1";
+let results: string;
+let options = {
+    method: 'GET',
+};
+let welcometext = document.getElementById("WelcomeDiv");
+let placeholder = 1755533657027;
+let date = new Date(placeholder * 1000);
+
+
+async function getStats() {
+    try {
+
+        const f = await fetch(url, options);
+        if (!f.ok) {
+            throw new Error(`Response status: ${f.status}`);
+        }
+        const JsonThing = await f.json();
+
+        results = JSON.stringify(JsonThing);
+
+        let e = JSON.parse(results);
+
+        if (welcometext && e) {
+
+            welcometext.innerText = e.updated_at;
+        }
+        else if (welcometext) {
+            welcometext.innerText = "Couldn't fetch last update time ;("
+        }
+        else {
+            throw new Error("Can't get desired div.");
+        }
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+
+
+}
+//getStats();
--- src/webgl.ts
+++ src/webgl.ts
@@ -1,9 +1,16 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { color } from 'three/tsl';
 
 const scene = new THREE.Scene();
 const loader = new OBJLoader();
-var theTeaPot;
+const colorForOBJ = "rgb(230, 171, 11)";
+console.log(colorForOBJ);
+//Adapt size to css width and height.
+const sizesForWebGLCanvas = getComputedStyle(document.documentElement);
+
+let theTeaPot: THREE.Group<THREE.Object3DEventMap>;
+
 const camera = new THREE.PerspectiveCamera(75,
     window.innerWidth / window.innerHeight,
     0.1,
@@ -11,26 +18,24 @@ const camera = new THREE.PerspectiveCamera(75,
 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, 
-    window.innerHeight
+renderer.setSize(parseInt(sizesForWebGLCanvas.getPropertyValue("--width-webgl")),
+    parseInt(sizesForWebGLCanvas.getPropertyValue("--height-webgl"))
 );
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-const cube = new THREE.Mesh(geometry, material);
+const material = new THREE.MeshBasicMaterial({ color: colorForOBJ})
+//const cube = new THREE.Mesh(geometry, material);
 //scene.add(cube);
-loader.load('./teapot.obj', function (obj){
-    const material2 = new THREE.MeshBasicMaterial({color: 0x00ff00})
+loader.load('./teapot.obj', function (obj) {
     obj.traverse(function (child) {
         if (child instanceof THREE.Mesh) {
-            child.material = material2;
+            child.material = material;
         }
     })
     theTeaPot = obj;
     scene.add(obj);
 });
-camera.position.z = 15;
+camera.position.z = 10;
 camera.rotateZ(-0.15)
 function animate() {
     renderer.render(scene, camera);
